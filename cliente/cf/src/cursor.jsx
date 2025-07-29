b21b4ea24b88144a4e03a8c501e2d1110eb518be
lucasmquinteros <lucasmartiniquinteros@gmail.com>
2025-07-29 15:55:42 -0300

    Terminar el control del mouse

--- cliente/cf/src/cursor.jsx
+++ cliente/cf/src/cursor.jsx
@@ -8,6 +8,25 @@ export default function Cursor() {
     return () => setBorder("border-red-500");
   }, []);
 
+  const [pos, setPos] = useState({
+    x: 0,
+    y: 0,
+  });
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPos({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <div
       className={`${border} z-[0] top-0 left-0 bg-gray-900 absolute m-5 w-20 h-20 rounded-full border transition`}
