776dcbefa3a7b1930775f0a4cf53a52a17f5f29d
Dante07 <dante.orsetti@gmail.com>
2025-08-01 17:43:32 -0300

    Icono que sigue el cursor

--- front/cf/src/cursor.jsx
+++ front/cf/src/cursor.jsx
@@ -10,13 +10,21 @@ export default function Cursor() {
   }, []);
 
   const [pos, setPos] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
-    // api window.client coordenadas del cursor, acomodar el icono segun la posicion
+    const handleListener = (e) => {
+      setPos({ x: e.clientX, y: e.clientY });
+    };
+
+    document.addEventListener("mousemove", handleListener);
+
+    return document.removeEventListener("mousemove", handleListener);
   }, []);
 
   return (
     <div
-      className={`${border} z-[-1] top-0 left-0 bg-blue-700 absolute w-20 h-20 border-2 rounded-full transition duration-200`}
+      style={{ transform: `translate(${pos.x})px, ${pos.y}px` }}
+      className={`${border} z-[-1] -top-10 -left-10 bg-blue-700 absolute w-20 h-20 border-2 rounded-full transition duration-200`}
     ></div>
   );
 }
